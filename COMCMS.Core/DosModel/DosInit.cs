066fa317dab582bc62a031dc654ac1ab8cc77d8e
congxin <zhao0876@188.com>
2020-02-28 12:50:37 +0800

    添加了dosorm的引用 删除了webtest和xcode生成器

---
+++ COMCMS.Core/DosModel/DosInit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using COMCMS.Common;
+using Dos.ORM;
+
+namespace COMCMS.Core
+{
+    public static class DB
+    {
+        public static readonly DbSession Context = new DbSession(DatabaseType.MySql, Utils.Configuration["connectionStrings:dbconn:connectionString"]);
+
+    }
+}
